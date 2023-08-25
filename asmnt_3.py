ae8626703c60809e310234d36f06b5cb437f49fa
Md Mohaiminul islam <142270026+Himu1993@users.noreply.github.com>
2023-08-25 22:43:46 +0200

    Add files via upload

---
+++ asmnt_3.py
@@ -0,0 +1,72 @@
+fruits_name =["apple" , "banana" , "cherry"]
+
+print (fruits_name)
+
+fruits_name.append("orange") #append can add single value
+print(fruits_name)
+
+item_list = [1,2,3,4,5]
+item_list.remove(3) #remove or pop
+print(item_list)
+
+color_list = ['red','green','blue']
+print(color_list)
+
+
+color_list.insert(1,'yellow')
+print(color_list)
+
+letter_c =['a','b','c', 'a', 'd']
+
+count = letter_c.count('a') #count
+print(count)
+
+list_data = [10,20,30,40,50]
+list_data.remove(50) #remove
+print(list_data)
+ 
+list_data.append(50) #return case can add append 
+print(list_data)
+
+values = [4,1,7,3,9]
+
+values.sort()
+print(values)
+
+cities =['new york', 'Los Angeles', 'chicago' , 'Houston' ]
+
+index =cities.index ('chicago') #index inform where the value is 
+print(index)
+
+animals = ['dog' , 'cat' , 'elephant' , 'lion']
+
+index =animals.index('elephant') #index find the value
+animals[index]='tiger'  #[animal[index] replace the value]
+print(animals)
+
+numbers= [5,2,8,2,1]
+
+count = numbers.count(2)
+print(count)
+
+fruits =['apple' ,'banana', 'cherry']
+
+reverse = fruits.reverse()
+print("reverse" , fruits)
+
+fruits.remove("apple")
+
+print(fruits)
+
+fruits.append("apple")
+print(fruits)
+
+#assighnment 2 .. character exixst or not
+Number = 12345
+
+chrc_check ="3"
+number_str = str(3)
+print(number_str)
+
+
+
